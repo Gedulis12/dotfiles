61911b5052518d9b0335cc323c4f1a22a0c990a4
Gedas <gediminas.repecka@gmail.com>
2024-01-08 18:03:17 +0200

    update

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -4,8 +4,8 @@ function ColorMyPencils(color)
     color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
 
---    vim.api.nvim_set_hl(0, "normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "normalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "normalFloat", { bg = "none" })
 end
 
  ColorMyPencils()
