be810c4b91d5ec2cc4b9bc9da3f94e6b59ae43e9
Gediminas Repečka <grepecka@drivewealth.com>
2025-11-04 22:29:35 +0200

    update

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -13,7 +13,7 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'gopls', 'pylsp', 'bashls', 'tailwindcss', 'templ'}
+local servers = { 'clangd', 'gopls', 'pylsp', 'bashls', 'tailwindcss', 'templ', 'java_language_server'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
